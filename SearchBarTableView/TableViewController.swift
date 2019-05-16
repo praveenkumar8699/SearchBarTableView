b3ef0e57cd02605e8821bfe21cdfbeeed23f4f63
Praveen Kumar <praveenkumar8699@gmail.com>
2019-05-16 14:08:45 +0530

    asdasd

---
+++ SearchBarTableView/TableViewController.swift
@@ -0,0 +1,128 @@
+//
+//  TableViewController.swift
+//  SearchBarTableView
+//
+//  Created by praveen Kumar on 16/05/19.
+//  Copyright © 2019 praveen Kumar. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController,UISearchResultsUpdating {
+    
+    //declaring global variables
+    var searchController : UISearchController!
+    var actors : [String]!
+    var result : [String]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        actors = ["Rashmika Mandanna","Raashi Khanna","Kajal Aggarwal","Kriti Sanon","Kiara Advani","Alia Bhat","Priyanka Chopra","Deepika Padukone"]
+        
+        searchController = UISearchController(searchResultsController: nil)
+        
+        tableView.tableHeaderView = searchController.searchBar
+        
+        searchController.searchResultsUpdater = self
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        //print(searchController.searchBar.text!)
+        
+        let predicate = NSPredicate(format:"SELF contains[c] %@",searchController.searchBar.text!)
+        
+        result = (actors as NSArray).filtered(using: predicate) as? [String]
+        
+        tableView.reloadData()
+        
+        print(result!)
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        var rows = 0
+        
+        if(searchController.isActive) {
+            rows = result.count
+        } else {
+            rows = actors.count
+        }
+        
+        return rows
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        if(searchController.isActive) {
+            cell.textLabel?.text = result[indexPath.row]
+        } else {
+            cell.textLabel?.text = actors[indexPath.row]
+        }
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
